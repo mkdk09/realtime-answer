fb7c743bcf25a6181ad1e529643672fb2c4022ec
mkdk09 <mkdk099@gmail.com>
2020-07-22 13:50:15 +0900

    ルーティングの設定

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import UseHome from '@/views/UserHome.vue'
+import AdminHome from '@/views/AdminHome.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'userHome',
+            component: UserHome
+        },
+        {
+            path: '/admin',
+            name: 'AdminHome',
+            component: AdminHome
+        }
+    ]
+})
