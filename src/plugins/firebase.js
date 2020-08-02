8369b2eaac0e4a086f8b66f0f3960b5cc12a87ee
mkdk09 <mkdk099@gmail.com>
2020-08-02 16:42:08 +0900

    fix bug

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -7,14 +7,14 @@ import 'firebase/auth'
 Vue.use(firestorePlugin)
 
 const config = {
-    apiKey: "AIzaSyBl6A8rwS8LtZiBkapftLTt9lVAhdE7YRU",
-    authDomain: "realtime-answer-aac8e.firebaseapp.com",
-    databaseURL: "https://realtime-answer-aac8e.firebaseio.com",
-    projectId: "realtime-answer-aac8e",
-    storageBucket: "realtime-answer-aac8e.appspot.com",
-    messagingSenderId: "729477940909",
-    appId: "1:729477940909:web:b9fd95ae5189c0988ce824",
-    measurementId: "G-H6ZW2V1LT8"
+  apiKey: 'AIzaSyBl6A8rwS8LtZiBkapftLTt9lVAhdE7YRU',
+  authDomain: 'realtime-answer-aac8e.firebaseapp.com',
+  databaseURL: 'https://realtime-answer-aac8e.firebaseio.com',
+  projectId: 'realtime-answer-aac8e',
+  storageBucket: 'realtime-answer-aac8e.appspot.com',
+  messagingSenderId: '729477940909',
+  appId: '1:729477940909:web:b9fd95ae5189c0988ce824',
+  measurementId: 'G-H6ZW2V1LT8'
 }
 
 const firebaseApp = firebase.initializeApp(config)
